0fa7c4cb66f5b7bb24ff7154a85a3b0a92a7e6f4
serdar <yigitserdar34@gmail.com>
2019-10-28 13:21:24 +0300

    list fetched by axios on screen

--- src/App.js
+++ src/App.js
@@ -1,13 +1,66 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
+import Axios from "axios";
 import Query from "./Query";
 
-function App() {
-  return (
-    <div className="App">
-      <Query />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      latlong: "",
+      venues: []
+    };
+  }
+
+  componentDidMount() {
+    this.getLocation();
+  }
+
+  getLocation = () => {
+    navigator.geolocation.getCurrentPosition(response => {
+      this.setState(
+        {
+          latlong: response.coords.latitude + "," + response.coords.longitude
+        },
+        () => {
+          this.getVenues("");
+        }
+      );
+    });
+  };
+
+  getVenues = query => {
+    const endPoint = "https://api.foursquare.com/v2/venues/explore?";
+    const params = {
+      client_id: "CS1DCR3PAJITL25EF3FJHAZB5LE1OD5PADOGFYUF5IQM0XO0",
+      client_secret: "VRLSN051WJVNKM1K1RR0T5RKZQO52MFLLRCXPLMFLKHYTLU5",
+      ll: this.state.latlong,
+      query: query,
+      v: "20192810",
+      limit: "10",
+      sortByDistance: "1"
+    };
+    Axios.get(endPoint + new URLSearchParams(params)).then(response => {
+      this.setState({ venues: response.data.response.groups[0].items });
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <Query getVenues={this.getVenues} />
+        <ul>
+          {this.state.venues.map(venue => {
+            return (
+              <li key={venue.venue.name}>
+                {venue.venue.name} Location: {venue.venue.location.address}
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/Query.js
+++ src/Query.js
@@ -1,14 +1,34 @@
 import React, { Component } from "react";
 
-export default class Query extends Component {
+class Query extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      searchInput: ""
+    };
+  }
+
+  newSearcher = event => {
+    this.setState({ searchInput: event.target.value });
+  };
   render() {
     return (
       <div>
-        <form>
-          <input type="search" placeholder="Search in exp: pub"></input>
-          <button>Search</button>
-        </form>
+        <input
+          onChange={this.newSearcher}
+          type="text"
+          placeholder="Search"
+          name="searchInput"
+          id="searchInput"
+        />
+        <button
+          onClick={this.props.getVenues.bind(null, this.state.searchInput)}
+        >
+          Search
+        </button>
       </div>
     );
   }
 }
+export default Query;
