c132e759afdb0a76f28cc3afb19be1fbd39f5654
serdar <yigitserdar34@gmail.com>
2020-05-22 12:40:29 +0300

    Updates

---
+++ precache-manifest.484f480d1aed3554440bdc730944f203.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95e820d43e5f06c5c1772614db59a7ee",
+    "url": "/FindApp/index.html"
+  },
+  {
+    "revision": "532dddbf633f6efd7a06",
+    "url": "/FindApp/static/css/main.1b461861.chunk.css"
+  },
+  {
+    "revision": "8079b22f18087faeb70a",
+    "url": "/FindApp/static/js/2.f34f5894.chunk.js"
+  },
+  {
+    "revision": "30ed4cbded537a8aab9c2be7d661ac22",
+    "url": "/FindApp/static/js/2.f34f5894.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "532dddbf633f6efd7a06",
+    "url": "/FindApp/static/js/main.7acccb02.chunk.js"
+  },
+  {
+    "revision": "6385086c13b24d2b5220",
+    "url": "/FindApp/static/js/runtime-main.179f62f8.js"
+  }
+]);
